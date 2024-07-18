75561ee7b0f39ff5a2b289f778f02198587cacb1
MoritzSchwerer <moritz.schwerer@gmx.de>
2024-07-18 11:03:03 +0200

    added some utils and made functions take const reference to GameState instead of copy

--- moves.hpp
+++ moves.hpp
@@ -2,118 +2,150 @@
 
 #include <vector>
 #include <iostream>
+#include <immintrin.h>
 
 #include "types.hpp"
 #include "constants.hpp"
 
+// #define SquareOf(X) _tzcnt_u64(X)
+#define Bitloop(X) for(;X; X = _blsr_u64(X))
+
+inline uint64_t SquareOf(uint64_t x) {
+    return _tzcnt_u64(x);
+}
+
+// this should set all bits to the same value
+// as the first bit
+inline Bitboard broadcastBit(Bitboard number) {
+    return -(number & 1ull) | (number & 1ull);
+}
+
+inline Bitboard broadcastSingleToMask(Bitboard number) {
+    const uint64_t shifted = number >> SquareOf(number);
+    return broadcastBit(shifted);
+}
+
 template<bool isWhite>
-constexpr Bitboard getEnemyPieces(GameState state) {
+constexpr Bitboard getEnemyPieces(const GameState &state) {
     if constexpr (isWhite) return state.b_pawn | state.b_rook | state.b_knight | state.b_bishop | state.b_queen | state.b_king;
     else return state.w_pawn | state.w_rook | state.w_knight | state.w_bishop | state.w_queen | state.w_king;
 }
 template<bool isWhite>
-constexpr Bitboard getFriendlyPieces(GameState state) {
+constexpr Bitboard getFriendlyPieces(const GameState &state) {
     if constexpr (isWhite) return state.w_pawn | state.w_rook | state.w_knight | state.w_bishop | state.w_queen | state.w_king;
     else return state.b_pawn | state.b_rook | state.b_knight | state.b_bishop | state.b_queen | state.b_king;
 }
 
 template<bool isWhite>
-constexpr Bitboard getPawns(GameState state) {
+constexpr Bitboard getPawns(const GameState &state) {
     if constexpr (isWhite) return state.w_pawn;
     else return state.b_pawn;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyPawns(GameState state) {
+constexpr Bitboard getEnemyPawns(const GameState &state) {
     if constexpr (isWhite) return state.b_pawn;
     else return state.w_pawn;
 }
 
 template<bool isWhite>
-constexpr Bitboard getKnights(GameState state) {
+constexpr Bitboard getKnights(const GameState &state) {
     if constexpr (isWhite) return state.w_knight;
     else return state.b_knight;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyKnights(GameState state) {
+constexpr Bitboard getEnemyKnights(const GameState &state) {
     if constexpr (isWhite) return state.b_knight;
     else return state.w_knight;
 }
 
 template<bool isWhite>
-constexpr Bitboard getBishops(GameState state) {
+constexpr Bitboard getBishops(const GameState &state) {
     if constexpr (isWhite) return state.w_bishop;
     else return state.b_bishop;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyBishops(GameState state) {
+constexpr Bitboard getEnemyBishops(const GameState &state) {
     if constexpr (isWhite) return state.b_bishop;
     else return state.w_bishop;
 }
 
 template<bool isWhite>
-constexpr Bitboard getRooks(GameState state) {
+constexpr Bitboard getRooks(const GameState &state) {
     if constexpr (isWhite) return state.w_rook;
     else return state.b_rook;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyRooks(GameState state) {
+constexpr Bitboard getEnemyRooks(const GameState &state) {
     if constexpr (isWhite) return state.b_rook;
     else return state.w_rook;
 }
 
 template<bool isWhite>
-constexpr Bitboard getQueens(GameState state) {
+constexpr Bitboard getQueens(const GameState &state) {
     if constexpr (isWhite) return state.w_queen;
     else return state.b_queen;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyQueens(GameState state) {
+constexpr Bitboard getEnemyQueens(const GameState &state) {
     if constexpr (isWhite) return state.b_queen;
     else return state.w_queen;
 }
 
 template<bool isWhite>
-constexpr Bitboard getKing(GameState state) {
+constexpr Bitboard getKing(const GameState &state) {
     if constexpr (isWhite) return state.w_king;
     else return state.b_king;
 }
 
 template<bool isWhite>
-constexpr Bitboard getEnemyKing(GameState state) {
+constexpr Bitboard getEnemyKing(const GameState &state) {
     if constexpr (isWhite) return state.b_king;
     else return state.w_king;
 }
 
 template<bool isWhite>
-constexpr Bitboard SecondLastRank() {
+constexpr Bitboard secondLastRank() {
     if constexpr (isWhite) return RANK_7;
     else return RANK_2;
 }
 
 template<bool isWhite>
-constexpr Bitboard PawnPush1(Bitboard pawns) {
+constexpr Bitboard secondRank() {
+    if constexpr (isWhite) return RANK_2;
+    else return RANK_7;
+}
+
+template<bool isWhite>
+constexpr Bitboard lastRank() {
+    if constexpr (isWhite) return RANK_8;
+    else return RANK_1;
+}
+
+template<bool isWhite>
+constexpr Bitboard pawnPush1(Bitboard pawns) {
     if constexpr (isWhite) return pawns << 8;
     else return pawns >> 8;
 }
 
 template<bool isWhite>
-constexpr Bitboard PawnPush2(Bitboard pawns) {
+constexpr Bitboard pawnPush2(Bitboard pawns) {
     if constexpr (isWhite) return pawns << 16;
     else return pawns >> 16;
 }
 
 template<bool isWhite>
-constexpr Bitboard PawnAttackLeft(Bitboard pawns) {
+constexpr Bitboard pawnAttackLeft(Bitboard pawns) {
     if constexpr (isWhite) return pawns << 7;
     else return pawns >> 9;
 }
+
 template<bool isWhite>
-constexpr Bitboard PawnAttackRight(Bitboard pawns) {
+constexpr Bitboard pawnAttackRight(Bitboard pawns) {
     if constexpr (isWhite) return pawns << 9;
     else return pawns >> 7;
 }

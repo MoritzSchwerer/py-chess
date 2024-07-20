77d8e6e6099aa9c969957f4bf8de22ff8a0e2977
MoritzSchwerer <moritz.schwerer@gmx.de>
2024-07-20 17:21:30 +0200

    wrote ChessGameEnv::step function to perform a move

--- main.cpp
+++ main.cpp
@@ -1,20 +1,303 @@
-#include <vector>
 #include <iostream>
-#include <bitset>
+#include <string>
+#include <array>
+#include <exception>
 
 #include "types.hpp"
 #include "movegen.hpp"
+#include "utils.hpp"
+#include "parsesan.hpp"
 
+typedef uint16_t Action;
+
+// TODO: this is not set in stone
+struct ChessObservation {
+    std::array<bool, 7104> observation;
+    std::array<bool, 4672> actionMask;
+    bool isTerminated;
+    bool isTruncated;
+};
+
+class ChessGameEnv {
+public:
+    GameState state;
+
+    ChessGameEnv() {}
+    ChessGameEnv(const std::string &fen) : state(parseFen(fen)) {}
+
+    Moves getPossibleMoves() const { return getLegalMoves(state); }
+
+    void step(const Action action);
+};
+
+constexpr std::array<int8_t, 73> planeToOffsetWhite = {
+    /* queen moves */ 8, 16, 24, 32, 40, 48, 56, 9, 18, 27, 36, 45, 54, 63, 1, 2, 3, 4, 5, 6, 7, -7, -14, -21, -28, -35, -42, -49, -8, -16, -24, -32, -40, -48, -56, -9, -18, -27, -36, -45, -54, -63, -1, -2, -3, -4, -5, -6, -7, 7, 14, 21, 28, 35, 42, 49,
+    /* knight moves */ 15, 17, 10, -6, -15, -17, 6, -10,
+    /* pawn moves (L, P, R) (N, B, R)*/ 7, 7, 7, 8, 8, 8, 9, 9, 9
+};
+constexpr std::array<int8_t, 73> planeToOffsetBlack = {
+    /* queen moves */ 8, 16, 24, 32, 40, 48, 56, 9, 18, 27, 36, 45, 54, 63, 1, 2, 3, 4, 5, 6, 7, -7, -14, -21, -28, -35, -42, -49, -8, -16, -24, -32, -40, -48, -56, -9, -18, -27, -36, -45, -54, -63, -1, -2, -3, -4, -5, -6, -7, 7, 14, 21, 28, 35, 42, 49,
+    /* knight moves */ 15, 17, 10, -6, -15, -17, 6, -10,
+    /* pawn moves (L, P, R) (N, B, R)*/ -9, -9, -9, -8, -8, -8, -7, -7, -7
+};
+
+template<bool isWhite>
+PieceType getPieceType(const GameState& state, uint8_t square) {
+    const Bitboard board = 1ull << square;
+    if constexpr (isWhite) {
+        if (state.w_pawn & board) return PieceType::Pawn;
+        if (state.w_knight & board) return PieceType::Knight;
+        if (state.w_bishop & board) return PieceType::Bishop;
+        if (state.w_rook & board) return PieceType::Rook;
+        if (state.w_queen & board) return PieceType::Queen;
+        if (state.w_king & board) return PieceType::King;
+    } else {
+        if (state.b_pawn & board) return PieceType::Pawn;
+        if (state.b_knight & board) return PieceType::Knight;
+        if (state.b_bishop & board) return PieceType::Bishop;
+        if (state.b_rook & board) return PieceType::Rook;
+        if (state.b_queen & board) return PieceType::Queen;
+        if (state.b_king & board) return PieceType::King;
+    }
+    return PieceType::None;
+}
+
+// this will throw if we can't find a match
+template<bool isWhite>
+Bitboard& getBitboardFromSquare(GameState& state, Bitboard board) {
+    if constexpr (isWhite) {
+        if (state.w_pawn & board) return state.w_pawn;
+        if (state.w_knight & board) return state.w_knight;
+        if (state.w_bishop & board) return state.w_bishop;
+        if (state.w_rook & board) return state.w_rook;
+        if (state.w_queen & board) return state.w_queen;
+        if (state.w_king & board) return state.w_king;
+    } else {
+        if (state.b_pawn & board) return state.b_pawn;
+        if (state.b_knight & board) return state.b_knight;
+        if (state.b_bishop & board) return state.b_bishop;
+        if (state.b_rook & board) return state.b_rook;
+        if (state.b_queen & board) return state.b_queen;
+        if (state.b_king & board) return state.b_king;
+    }
+    throw std::runtime_error("Error: getBitboardFromSquare couldn't find a match for the square.");
+}
+
+template<bool isWhite>
+Bitboard& getBitboardFromPieceType(GameState& state, PieceType type) {
+    if constexpr (isWhite) {
+        if (type == PieceType::Pawn) return state.w_pawn;
+        if (type == PieceType::Knight) return state.w_knight;
+        if (type == PieceType::Bishop) return state.w_bishop;
+        if (type == PieceType::Rook) return state.w_rook;
+        if (type == PieceType::Queen) return state.w_queen;
+        if (type == PieceType::King) return state.w_king;
+    } else {
+        if (type == PieceType::Pawn) return state.b_pawn;
+        if (type == PieceType::Knight) return state.b_knight;
+        if (type == PieceType::Bishop) return state.b_bishop;
+        if (type == PieceType::Rook) return state.b_rook;
+        if (type == PieceType::Queen) return state.b_queen;
+        if (type == PieceType::King) return state.b_king;
+    }
+    throw std::runtime_error("Error: getBitboardFromSquare couldn't find a match for the square.");
+}
+
+// returns if we actually removed a piece
+template<bool isWhite>
+bool removeEnemyPiece(GameState& state, Bitboard targetBoard) {
+    if constexpr (isWhite) {
+        state.b_pawn &= ~targetBoard;
+        state.b_knight &= ~targetBoard;
+        state.b_bishop &= ~targetBoard;
+        state.b_rook &= ~targetBoard;
+        state.b_queen &= ~targetBoard;
+        state.b_king &= ~targetBoard;
+        return state.b_pawn & targetBoard | state.b_knight & targetBoard | state.b_bishop & targetBoard | state.b_rook & targetBoard | state.b_queen & targetBoard | state.b_king & targetBoard;
+    } else {
+        state.w_pawn &= ~targetBoard;
+        state.w_knight &= ~targetBoard;
+        state.w_bishop &= ~targetBoard;
+        state.w_rook &= ~targetBoard;
+        state.w_queen &= ~targetBoard;
+        state.w_king &= ~targetBoard;
+        return state.w_pawn & targetBoard | state.w_knight & targetBoard | state.w_bishop & targetBoard | state.w_rook & targetBoard | state.w_queen & targetBoard | state.w_king & targetBoard;
+    }
+}
+
+template<bool isWhite>
+inline bool isCastle(uint8_t ss, uint8_t ts) {
+    if constexpr (isWhite) {
+        return ss == 4 && (ts == 0 || ts == 7);
+    } else {
+        return ss == 60 && (ts == 56 || ts == 63);
+    }
+}
+
+template<bool isWhite>
+void handleCastling(GameState& state, uint8_t sourceSquare, uint8_t targetSquare) {
+    // handle casteling
+    if constexpr (isWhite) {
+        // left castle
+        if (sourceSquare > targetSquare) {
+            state.w_king =   0b00000100ull;
+            state.w_rook &= ~0b00000001ull;
+            state.w_rook |=  0b00001000ull;
+        } else {
+            state.w_king =   0b01000000ull;
+            state.w_rook &= ~0b10000000ull;
+            state.w_rook |=  0b00100000ull;
+        }
+    } else {
+        if (sourceSquare > targetSquare) {
+            state.w_king =   0b00000100ull << 56;
+            state.w_rook &= ~0b00000001ull << 56;
+            state.w_rook |=  0b00001000ull << 56;
+        } else {
+            state.w_king =   0b01000000ull << 56;
+            state.w_rook &= ~0b10000000ull << 56;
+            state.w_rook |=  0b00100000ull << 56;
+        }
+    }
+    state.status.removeCastlingRights<isWhite>();
+}
+
+template<bool isWhite>
+void updateCastlingRights(GameState& state, Bitboard sourceBoard, PieceType type) {
+    // update castling rights king moves
+    if (type == PieceType::King) {
+        state.status.removeCastlingRights<isWhite>();
+    }
+
+    // update castling right rook moves
+    if (type == PieceType::Rook) {
+        if (sourceBoard & initialRookLeft<isWhite>() ) state.status.removeCastlingRightsLeft<isWhite>();
+        if (sourceBoard & initialRookRight<isWhite>()) state.status.removeCastlingRightsRight<isWhite>();
+    }
+}
+
+template<bool isWhite>
+void handlePromotion(GameState& state, Bitboard& pieceBoard, Bitboard targetBoard, PieceType promotion, PieceType type) {
+    if (type == PieceType::Pawn && targetBoard & lastRank<isWhite>()) {
+        Bitboard& promotionBoard = getBitboardFromPieceType<isWhite>(state, promotion);
+        promotionBoard |= targetBoard;
+    } else {
+        pieceBoard |= targetBoard;
+    }
+}
+
+template<bool isWhite>
+void handleEnpassantCapture(GameState& state, Bitboard targetBoard) {
+    removeEnemyPiece<isWhite>(state, pawnPush1<!isWhite>(targetBoard));
+}
+
+template<bool isWhite>
+bool updateGameState(GameState& state, uint8_t sourceSquare, uint8_t targetSquare, PieceType promotion) {
+    const GameStatus& status = state.status;
+    const Bitboard sourceBoard = 1ull << sourceSquare;
+    const Bitboard targetBoard = 1ull << targetSquare;
+
+    const PieceType type = getPieceType<isWhite>(state, sourceSquare);
+    const bool isEnpassantPossible = state.status.enpassant;
+    state.status.disableEnpassant();
+
+    if (isCastle<isWhite>(sourceSquare, targetSquare)) {
+        handleCastling<isWhite>(state, sourceSquare, targetSquare);
+        return false;
+    }
+
+    updateCastlingRights<isWhite>(state, sourceBoard, type);
+
+    // remove piece from source position
+    Bitboard& pieceBoard = getBitboardFromSquare<isWhite>(state, sourceBoard);
+    pieceBoard &= ~sourceBoard;
+
+    // handle promotion
+    handlePromotion<isWhite>(state, pieceBoard, targetBoard, promotion, type);
+
+    // handle enpassant take
+    if (type == PieceType::Pawn && isEnpassantPossible && targetBoard & state.enpassant_board) {
+        handleEnpassantCapture<isWhite>(state, targetBoard);
+        return true;
+    }
+
+    // handle enable enpassant
+    if (enablesEnpassant<isWhite>(state, sourceBoard, targetBoard, type)) {
+        state.enpassant_board = pawnPush1<isWhite>(sourceBoard);
+        state.status.enableEnpassant();
+        return true;
+    }
+
+
+    const bool tookEnemyPiece = removeEnemyPiece<isWhite>(state, targetBoard);
+    return tookEnemyPiece || type == PieceType::Pawn;
+}
+
+PieceType getPromotion(uint8_t plane) {
+    if (plane < 64) return PieceType::Queen;
+    switch ((plane - 64) % 3) {
+        case 0: return PieceType::Knight;
+        case 1: return PieceType::Bishop;
+        case 2: return PieceType::Rook;
+        default: return PieceType::None;
+    }
+}
+
+template<bool isWhite>
+bool enablesEnpassant(GameState& state, Bitboard sourceBoard, Bitboard targetBoard, PieceType type) {
+    const Bitboard enemyPawns = getEnemyPawns<isWhite>(state);
+    return (
+        type == PieceType::Pawn &&
+        targetBoard & pawnPush2<isWhite>(sourceBoard) &&
+        (pawnAttackLeft<isWhite>(pawnPush1<isWhite>(sourceBoard)) & enemyPawns ||
+        pawnAttackRight<isWhite>(pawnPush1<isWhite>(sourceBoard)) & enemyPawns)
+    );
+}
+
+template<bool isWhite>
+void updateMoveCount(GameState& state, bool reset) {
+    if (reset) state.halfMoveClock = 0;
+    else state.halfMoveClock++;
+
+    if constexpr (!isWhite) state.fullMoveCount++;
+}
+
+template<bool isWhite>
+uint8_t getOffsetFromPlane(uint8_t plane) {
+    if constexpr (isWhite) return planeToOffsetWhite[plane];
+    else planeToOffsetWhite[plane];
+}
+
+template<bool isWhite>
+void makeMove(GameState& state, Action action) {
+    const uint8_t sourceSquare = action / 73;
+    const uint8_t plane = action % 73;
+    const int8_t offset = getOffsetFromPlane<isWhite>(plane);
+    const uint8_t targetSquare = sourceSquare + offset;
+
+    const PieceType promotion = getPromotion(plane);
+
+    bool resetHalfMoveClock = updateGameState<isWhite>(state, sourceSquare, targetSquare, promotion);
+
+    updateMoveCount<isWhite>(state, resetHalfMoveClock);
+
+    state.status.silentMove();
+}
+
+// this will perform a move in the game
+// this is a no legality check version
+void ChessGameEnv::step(Action action) {
+    if (state.status.isWhite) makeMove<true>(state, action);
+    else makeMove<false>(state, action);
+}
 
 int main() {
-    GameState state;
-    // std::vector<Move> moves = get_legal_moves<true>(state);
-    // for (const Move& m : moves) {
-    //     print_move(m);
-    // }
-    // std::cout << "Number of possible moves detected: " << moves.size() << std::endl;
-    state.w_pawn = 0b00010000000000001110111100000000;
-    Bitboard seenSquares =  getSeenSquares<true>(state);
-    printBoard(state, seenSquares);
+    ChessGameEnv game;
+    game.step(8*73 + 1);
+    game.step(63*73 + 29);
+    game.step(24*73 + 0);
+    game.step(49*73 + 29);
+    game.step(32*73 + 7);
+
     return 0;
 }

93954a4c6bb0394530226d19121c586915cb85bc
MoritzSchwerer <moritz.schwerer@gmx.de>
2024-07-22 15:20:11 +0200

    wrote bind.cpp to create python module via pybind11

---
+++ bind.cpp
@@ -0,0 +1,35 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include "game_env.hpp" // assume this is the header file for the ChessGameEnv struct
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(chess, m) {
+
+    py::class_<ChessGameEnv> chess_env(m, "ChessGameEnv");
+
+    chess_env.def(py::init<>()); // default constructor
+
+    chess_env.def("step", &ChessGameEnv::step, py::arg("action"));
+    chess_env.def("observe", &ChessGameEnv::observe);
+
+    py::class_<ChessObservation> chess_observation(m, "ChessObservation");
+
+    chess_observation.def_readonly("whiteReward", &ChessObservation::whiteReward);
+    chess_observation.def_readonly("blackReward", &ChessObservation::blackReward);
+    chess_observation.def_readonly("isTerminated", &ChessObservation::isTerminated);
+
+    chess_observation.def_property_readonly("observation", 
+                                    [&m](const ChessObservation& co) {
+                                        py::array_t<bool> arr(co.observation.size());
+                                        std::copy(co.observation.begin(), co.observation.end(), arr.mutable_data());
+                                        return arr;
+                                    });
+
+    chess_observation.def_property_readonly("actionMask", 
+                                    [&m](const ChessObservation& co) {
+                                        py::array_t<bool> arr(co.actionMask.size());
+                                        std::copy(co.actionMask.begin(), co.actionMask.end(), arr.mutable_data());
+                                        return arr;
+                                    });
+}
